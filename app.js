7f8c7ec337a153f4bdb5f39930a5dc1afce863e4
ahsanatha <ahsanathallah@student.telkomuniversity.ac.id>
2020-12-17 12:14:43 +0700

    add cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 const dotenv = require("dotenv");
+var cors = require('cors')
 
 dotenv.config();
 
@@ -69,6 +70,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors())
 
 app.use("/", indexRouter);
 //admin
