3a443e0f816e6cb57acbf334bf5d541a9c4ed0eb
ahsanatha <ahsanathallah@student.telkomuniversity.ac.id>
2020-12-19 13:44:08 +0700

    fix validate user

--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,35 +1,35 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const verifyUser = (req, res, next) => {
-
-    const token = req.cookies.authCookie;
-
-    User.findOne({username: req.body.username})
-    .then((user) => {
+  const token = req.cookies.authCookie;
+  try {
+    if (!token) {
+      return res.status(401).json("You need to login");
+    }
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET_USER);
+    req.user = verified;
+    // next();
+    User.findById(req.user.id)
+      .then((user) => {
         if (user.isVerified == true) {
-            try {
-                if(!token) {
-                    return res.status(401).json('You need to login')
-                }
-        
-                const verified = jwt.verify(token, process.env.TOKEN_SECRET_USER);
-                req.user = verified;
-                next();
-            }
-            catch (err) {
-                try {
-                    const verified = jwt.verify(token, process.env.TOKEN_SECRET_SUPER_ADMIN);
-                    req.user = verified;
-                    next();
-                }
-                catch(err){
-                    return res.status(500).json(err);
-                }
-
-            }
+          next();
+        } else {
+          return res.status(401).json("please verify your email address");
         }
-    })
+      })
+      .catch((err) => {
+        return res.status(500).json(err);
+      });
+  } catch (err) {
+    try {
+      const verified = jwt.verify(token, process.env.TOKEN_SECRET_SUPER_ADMIN);
+      req.user = verified;
+      next();
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
 };
 
 module.exports = verifyUser;
